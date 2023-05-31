11a42cc71f109d2b2c40b49d4a5e15abcad86495
Andrei Shubich <AShubich@inspirebrands.com>
2023-05-31 19:51:26 +0300

    fix path

--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ app.use(cors({ origin: '*' }));
 
 app.use("/api", routes);
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 // Global error handling
